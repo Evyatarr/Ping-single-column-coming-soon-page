33928617cc22e30034efe538038e51f11a9b81d0
Evyatarr <evyatar210@gmail.com>
2022-10-30 01:50:00 +0300

    added js

---
+++ assests/script.js
@@ -0,0 +1,18 @@
+// function ValidateEmail(form) {
+//   const email_format = /^w+([.-]?w+)*@w+([.-]?w+)*(.w{2,3})+$/;
+//   if (!email_format.value.match(email_format)) {
+//     alert( "Please provide a valid email address");
+//   }
+
+// }
+
+
+function ValidateEmail(form) 
+{
+ if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(myForm.emailAddr.value))
+  {
+    return (true)
+  }
+    alert("You have entered an invalid email address!")
+    return (false)
+}
